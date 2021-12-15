35354efabbc6c31896e71c82cba3e848d6178a36
soangsoang <89063739+soangsoang@users.noreply.github.com>
2021-12-15 14:40:25 +0700

    testcodew

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hee luu Vợ anh, lại là ck em đây :))!',
+    introDesc: `Chúc Mình Sinh Nhật Bây Be, Anh có một câu hỏi cần em trả lời thật lòng đây!`,
+    btnIntro: 'oke nun :))',
+    title: 'Em có yêu anh không vậy taaaaaaaaaa 😙',
+    desc: 'Em mà trả lời sai là pay fb đó nha :)))) ',
+    btnYes: 'Yêu rất nhiều luônnn :((',
+    btnNo: 'Anh mơ đó à,đương nhiên là không rồi :)))',
+    question:'Được rồi anh cho em một cơ hội vì khi nảy chọn sai,nói lý do yêu anh đi :))',
+    btnReply: 'Gửi ck em <3',
+    reply: 'Tại vì ck thương em và ngầu vcl :)) (Đừng chống cự, em không chống được đâu)',
+    mess: 'Anh biết mà 🥰. (Em cũng đâu trả lời khác được) 😘😘',
+    messDesc: 'CHÚC MỪNG SINH NHẬT NGƯỜI CON GÁI ANH YÊU,CÒN CHỜ GÌ NỮA ĐẾN VỚI CK EM NÀO!!!',
+    btnAccept: 'Okeeeeeeeeeee :)))',
+    messLink: 'https://www.facebook.com/profile.php?id=100036576351409' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
